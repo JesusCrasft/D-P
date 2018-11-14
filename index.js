98b8841c73027068bcfc184a397bc7e9002ffd1b
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2018-11-14 10:54:37 -0400

    Se crearon los patos y se agregaron mas funciones y variables.

--- index.js
+++ index.js
@@ -3,17 +3,70 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var areah = canvas.height;
 var areaw = canvas.width;
+var puntos = 0;
+var disparo;
+var bucle;
+var patox = {
+	x : 50,
+	y : 50,
+	velocidad : 5,
+	img : document.createElement('img')	
+};
+var patoy = {
+	x : 100,
+	y : 190,
+	velocidad : 5,
+	img : document.createElement('img')	
+};
+var patoz = {
+	x : 100,
+	y : 190,
+	velocidad : 5,
+	img : document.createElement('img')	
+};
+patox.img.src = "img/pato.jpeg"
+patoy.img.src = "img/pato.jpeg"
+patoz.img.src = "img/pato.jpeg"
+
+//Clases
+
 
 //Funciones
-function dibujar() {
+function dibujarpx() {
+	ctx.drawImage(patox.img, patox.x, patox.y);
+}
+
+function dibujarpy() {
+	ctx.drawImage(patoy.img, patoy.x, patoy.y);
+}
+
+function dibujarpz() {
+	ctx.drawImage(patoz.img, patoz.x, patoz.y);
+}
+
+function actualizar() {
 	
 }
 
+function colisiones() {
+
+}
+
+function dibujar() {
+	dibujarpx()
+	dibujarpy()
+	dibujarpz()
+}
+
 function frame() {
-	
+	actualizar();
+	colisiones();
+	dibujar();
+	bucle = requestAnimationFrame(frame);	
 }
 
 function iniciar() {
 	var modal = document.getElementById('modal');
 	modal.style.display = 'none';
+	frame()
 }
