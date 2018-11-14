698b1d82b4e12a7299d4feba1a86ea5af987eacd
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2018-11-13 23:09:09 -0400

    Se agrego el boton de comienzo.Se agregaron la funcion de inicio.Se agrego una imagen de un boton

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+//Variables
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var areah = canvas.height;
 var areaw = canvas.width;
+
+//Funciones
+function dibujar() {
+	
+}
+
+function frame() {
+	
+}
+
+function iniciar() {
+	var modal = document.getElementById('modal');
+	modal.style.display = 'none';
+}
