fcacfc8f0800b5c2a83ac6cdcdb4fc8f5ce5259a
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2018-11-19 10:48:45 -0400

    Se crearo las primeras colisiones.Se agrego la clase Informacion.Se agregaron las TM(agregar un nombre,crear msjs).Y se arreglo el html.

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var ctx = canvas.getContext('2d');
 var areah = canvas.height;
 var areaw = canvas.width;
 var puntos = 0;
+var patos = 0;
+var ju1 = crearTMIP("Coloque el nombre de Jugador");
+var inst = crearTM("Disparele a tanto patos como puedas");
+var ju;
 var disparo;
 var bucle;
+var x;
+var y;
 var patox = {
 	x : 800,
 	y : 20,
@@ -50,9 +56,108 @@ patob.img.src = "img/pato.jpeg";
 patoc.img.src = "img/pato.jpeg";
 
 //Clases
+class Informacion {
+	constructor() {
+		
+	}
+	dibujar() {
+		ctx.fillStyle = 'black';
+		ctx.font = "18px Arial";
+		ctx.fillText("Puntos: "+puntos, 200, 15);
+		ctx.fillText("Jugador: "+ju, 20, 15);
+		ctx.fillText("Patos: "+patos, 300, 15);
+	}
+}
 
-
+var info = new Informacion();
 //Funciones
+function crearTM(msj) {
+	var f = document.createElement('div');
+	var m = document.createElement('div');
+	var t = document.createTextNode(msj); 
+	f.appendChild(m);
+	m.appendChild(t);
+	f.className = "contenedor";
+	m.className = "modal";
+	var cerrar = document.createElement('div');
+	var x = document.createTextNode("X");
+	cerrar.appendChild(x);
+	cerrar.className = "cerrar";
+	cerrar.addEventListener("click", function() {
+		f.style.visibility = "hidden";
+	});
+	m.appendChild(cerrar);
+	document.body.appendChild(f);
+	return f;
+}
+
+function crearTMIP(text, nombre) {
+	var f = document.createElement('div');
+	var m = document.createElement('div');
+	var inp = document.createElement('input'); 
+	var t = document.createTextNode(text);
+	inp.type = text;
+	inp.id = nombre;
+	inp.name = nombre;
+	f.appendChild(m);
+	m.appendChild(inp);
+	m.appendChild(t);
+	f.className = "contenedor";
+	m.className = "modal";
+	var cerrar = document.createElement('div');
+	var e = document.createTextNode("Enviar");
+	cerrar.appendChild(e);
+	cerrar.className = "cerrar";
+	cerrar.addEventListener("click", function() {
+		f.style.visibility = "hidden";
+		if (inp.value == false) {
+			ju = "Predeterminado";
+		} else {
+			ju = inp.value;
+		}
+		frame();
+	});
+	m.appendChild(cerrar);
+	document.body.appendChild(f);
+	return f;
+}
+
+function mostrarM(obj) {
+	obj.style.visibility = "visible";
+}
+
+function getMousePos(canvas, evt) {
+	return {
+		x: evt.clientX,
+  		y: evt.clientY
+   };
+}
+canvas.addEventListener('mousemove', function(evt) {
+   var mousePos = getMousePos(canvas, evt);
+   x = mousePos.x;
+   y = mousePos.y;
+}, false);
+
+function choquex() {
+	if (x > patox.x && x < patox.x+5 && y < 20) {
+		console.log("as");
+		puntos = puntos+1;
+	}
+}
+
+function choquey() {
+	if (x > patoy.x && x < patoy.x+5 && y < 20) {
+		console.log("as");
+	}
+}
+
+function choquez() {
+	if (x > patoz.x && x < patoz.x+5 && y < 20) {
+		console.log("as");
+	}
+}
+
+
 function moverpx() {
 	if (patox.x > 0) {	
 		patox.x -= patox.velocidad;
@@ -154,26 +259,27 @@ function dibujarpc() {
 }
 
 function actualizar() {
-	moverpx()
-	moverpy()
-	moverpz()
-	moverpa()
-	moverpb()
-	moverpc()	
+	moverpx();
+	moverpy();
+	moverpz();
+	moverpa();
+	moverpb();
+	moverpc();	
 }
 
 function colisiones() {
-
+	choquex();
 }
 
 function dibujar() {
 	ctx.clearRect(0,0,areaw, areah);	
-	dibujarpx()
-	dibujarpy()
-	dibujarpz()
-	dibujarpa()
-	dibujarpb()
-	dibujarpc()
+	dibujarpx();
+	dibujarpy();
+	dibujarpz();
+	dibujarpa();
+	dibujarpb();
+	dibujarpc();
+	info.dibujar();
 }
 
 function frame() {
