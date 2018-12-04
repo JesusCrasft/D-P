e56cff6defca041882a0ac7aebbc6082a754c538
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2018-12-04 10:52:47 -0400

    Se termino el 80% de las colisiones.Se agregaron las municiones.Se agrego la funcion FIN.Y se mejoraron las imagenes.

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ var areah = canvas.height;
 var areaw = canvas.width;
 var puntos = 0;
 var patos = 0;
+var puntosM = crearTM(puntos);
+var perdiste = crearTM("Game Over");
 var ju1 = crearTMIP("Coloque el nombre de Jugador");
 var inst = crearTM("Disparele a tanto patos como puedas.No dejes que se escapen");
 var ju;
 var disparo;
 var bucle;
+var municiones = 6;
+var premu = true;
 var x;
 var y;
 var patox = {
@@ -48,9 +52,9 @@ var patoc = {
 	velocidad : 5,
 	img : document.createElement('img')	
 };
-patox.img.src = "img/pato.jpeg";
-patoy.img.src = "img/pato.jpeg";
-patoz.img.src = "img/pato.jpeg";
+patox.img.src = "img/otap.png";
+patoy.img.src = "img/otap.png";
+patoz.img.src = "img/otap.png";
 patoa.img.src = "img/pato.jpeg";
 patob.img.src = "img/pato.jpeg";
 patoc.img.src = "img/pato.jpeg";
@@ -66,6 +70,7 @@ class Informacion {
 		ctx.fillText("Puntos: "+puntos, 400, 15);
 		ctx.fillText("Jugador: "+ju, 20, 15);
 		ctx.fillText("Patos: "+patos, 700, 15);
+		ctx.fillText("Disparos: "+municiones, 900, 15);
 	}
 }
 
@@ -138,47 +143,89 @@ canvas.addEventListener('mousemove', function(evt) {
    y = mousePos.y;
 }, false);
 
+function Municiones() {
+	if (municiones <= 0) {
+		premu = false;
+		setInterval(function () {
+			municiones = 6;
+			premu = true;
+		},3000);
+	}
+}
 
+function Fin() {
+	if (patos >= 30) {
+		patox.velocidad = 0;
+		patoy.velocidad = 0;
+		patoz.velocidad = 0;
+		patoa.velocidad = 0;
+		patob.velocidad = 0;
+		patoc.velocidad = 0;
+		patox.x = 1300;
+		patoy.x = 1200;
+		patoz.x = 1100;
+		patoa.x = -100;
+		patob.x = -200;
+		patoc.x = -300;		
+		mostrarM(puntosM);
+		mostrarM(perdiste);
+		setInterval(function () {
+			document.location.reload();
+		},5000);
+	}
+}
 
 function choquex() {
-	if (x > patox.x && patox.x > x-100 && y < 25 && y > 1) {
+	if (x > patox.x && patox.x > x-100 && y < 25 && y > 1 && premu == true) {
 		puntos = puntos+1;
 		patox.x = 1200;
+		municiones = municiones-1;
+		Municiones();
 	}
 }
 
 function choquey() {
-	if (x > patoy.x && patoy.x > x-100 && y < 25 && y > 1) {
+	if (x > patoy.x && patoy.x > x-100 && y < 25 && y > 1 && premu == true) {
 		puntos = puntos+1;
 		patoy.x = 1100;	
+		municiones = municiones-1;
+		Municiones();		
 	}
 }
 
 function choquez() {
-	if (x > patoz.x && patoz.x > x-100 && y < 25 && y > 1) {
+	if (x > patoz.x && patoz.x > x-100 && y < 25 && y > 1 && premu == true) {
 		puntos = puntos+1;
-		patoz.x = 1000;		
+		patoz.x = 1000;	
+		municiones = municiones-1;
+		Municiones();			
 	}
 }
 
 function choquea() {
-	if (x < patoa.x && patoa.x < x+50 && y > 170 && y < 210) {
+	if (x < patoa.x && patoa.x < x+50 && y > 170 && y < 210 && premu == true) {
 		puntos = puntos+1;
 		patoa.x = -300;
+		municiones = municiones-1;
+		Municiones();		
 	}
 }
 
 function choqueb() {
-	if (x < patob.x && patob.x < x+50 && y > 170 && y < 210) {
+	if (x < patob.x && patob.x < x+50 && y > 170 && y < 210 && premu == true) {
 		puntos = puntos+1;
 		patob.x = -200;
+		municiones = municiones-1;
+		Municiones();		
 	}	
 }
 
 function choquec() {
-	if (x < patoc.x && patoc.x < x+50 && y > 170 && y < 210) {
+	if (x < patoc.x && patoc.x < x+50 && y > 170 && y < 210 && premu == true) {
 		puntos = puntos+1;
 		patoc.x = -100;	
+		municiones = municiones-1;
+		Municiones();		
 	}	
 }
 
@@ -188,9 +235,9 @@ function moverpx() {
 	}
 	if (patox.x <= 0) {
 		patox.x = 1200;
-		patox.click = false;
 		patos = patos+1;
 		patox.velocidad = patox.velocidad+1;
+		Fin();
 	}	
 	if (patox.velocidad > 10) {
 		patox.velocidad = 5;
@@ -203,9 +250,9 @@ function moverpy() {
 	}
 	if (patoy.x <= 0) {
 		patoy.x = 1100;
-		patoy.click = false;
 		patos = patos+1;
 		patoy.velocidad = patoy.velocidad+1;
+		Fin();		
 	}	
 	if (patoy.velocidad > 10) {
 		patoy.velocidad = 5;
@@ -218,9 +265,9 @@ function moverpz() {
 	}
 	if (patoz.x <= 0) {
 		patoz.x = 1000;
-		patoz.click = false;
 		patos = patos+1;
 		patoz.velocidad = patoz.velocidad+1;
+		Fin();		
 	}	
 	if (patoz.velocidad > 10) {
 		patoz.velocidad = 5;
@@ -232,9 +279,9 @@ function moverpa() {
 	}
 	if (patoa.x > 999) {
 		patoa.x = -300;
-		patoa.click = false;
 		patos = patos+1;
 		patoa.velocidad = patoa.velocidad+1;
+		Fin();		
 	}
 	if (patoa.velocidad > 12) {
 		patoa.velocidad = 5;
@@ -247,9 +294,9 @@ function moverpb() {
 	}
 	if (patob.x > 999) {
 		patob.x = -200;
-		patob.click = false;
 		patos = patos+1;
 		patob.velocidad = patob.velocidad+1;
+		Fin();		
 	}
 	if (patob.velocidad > 12) {
 		patob.velocidad = 5;
@@ -262,9 +309,9 @@ function moverpc() {
 	}
 	if (patoc.x > 999) {
 		patoc.x = -100;
-		patoc.click = false;
 		patos = patos+1;
 		patoc.velocidad = patoc.velocidad+1;
+		Fin();		
 	}
 	if (patoc.velocidad > 12) {
 		patoc.velocidad = 5;
